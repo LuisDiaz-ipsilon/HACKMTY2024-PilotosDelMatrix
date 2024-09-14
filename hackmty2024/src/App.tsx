19457c9bf957b0c58f82f9f69fe7e9c140ca629b
aylin <aylindemetci@gmail.com>
2024-09-14 17:12:49 -0600

    fix vite tailwind css config issue

--- hackmty2024/src/App.tsx
+++ hackmty2024/src/App.tsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='grid grid-cols-4 grid-rows-6 min-h-screen min-w-screen'>
+        <div className='col-span-4 row-span-1 bg-indigo-600'>
+          <h1 className="text-3xl font-sans">
+            Hello world!
+          </h1>
+        </div>
+        <div className='col-span-4 row-span-5'>
+          
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
